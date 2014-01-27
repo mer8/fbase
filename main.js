8cd01a587932d5b053564473105c8c85809ccd86
Meredith Bryan <merbryan@gmail.com>
2014-01-27 07:00:07 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+angular.module("MyApp", ["firebase"])
+	.controller("MyController", function($scope, $firebase){
+		var issuesRef = new Firebase("https://lizardsarecool.firebaseio.com");
+
+		$scope.issues = $firebase(issuesRef);
+
+		$scope.addOne = function(){
+			issuesRef.push( {title:$scope.title, body:$scope.body} );
+		};
+	});
